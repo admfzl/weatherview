12ca0eb9375c29c4c60fbdd353ca1f2aea6d1e14
admfzl <133797071+admfzl@users.noreply.github.com>
2025-08-04 12:28:44 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🌤️ Starting WeatherView production server..."
-echo "📡 Connecting to database..."
-echo "🔄 Initializing weather data collection..."
-
-node dist/index.js
